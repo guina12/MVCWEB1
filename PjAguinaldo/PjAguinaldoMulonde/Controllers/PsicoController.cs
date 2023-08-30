169c31eab7887a13a86c3fadfdf6fe223b755e8f
8 owie <guinathetrap@gmail.com>
2023-08-30 20:48:16 -0300

    first commit

---
+++ PjAguinaldo/PjAguinaldoMulonde/Controllers/PsicoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PjAguinaldoMulonde.Controllers
+{
+    public class PsicoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PjAguinaldo/PjAguinaldoMulonde/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PjAguinaldoMulonde.Models;
+
+namespace PjAguinaldoMulonde.Data
+{
+    public class AppDbContext:IdentityDbContext<AppUser>
+    {
+        public AppDbContext()
+        {
+
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+      public  DbSet<Psico> psicos { get; set; }
+    }
+}
